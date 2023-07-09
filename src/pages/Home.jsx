37b03426e71310c4538bb4d2221af790407d3b7c
TetiPalii <TetianaPalii9491@gmail.com>
2023-07-09 14:21:36 +0200

    tsk_1

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,24 @@
 import { Container, CountryList, Heading, Loader, Section } from 'components';
+import { useEffect, useState } from 'react';
+import { getCountries } from 'service/country-service';
 
 export const Home = () => {
+  const [countries, setCountries] = useState([]);
+  useEffect(() => {
+    const fetchCountries = async () => {
+      try {
+        const response = await getCountries();
+        setCountries(response);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchCountries();
+  }, []);
   return (
     <Section>
       <Container>
-        <h2>Home</h2>
+        <CountryList countries={countries} />
       </Container>
     </Section>
   );
